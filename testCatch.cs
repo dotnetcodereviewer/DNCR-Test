9a5fe40a6b05edf4968ab221b69132dce5b59271
dotnetcodereviewer <dotnetcodereviewer.support@gmail.com>
2026-06-22 13:50:16 +0530

    Create testCatch.cs

---
+++ testCatch.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class C
+{
+    public void Mk()
+    {
+        try
+        {
+            Work();
+        }
+        catch (Exception ex)
+        {
+            // Handle exception
+        }
+    }
+}
